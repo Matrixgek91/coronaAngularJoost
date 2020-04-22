2528af1771eec5f91b897cdcac6e4360deb0427a
Matrixgek91 <jschouw1@gmail.com>
2020-04-22 14:24:03 +0200

    Added news feed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* Materials components imports */
+
+import { MatSliderModule } from '@angular/material/slider';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
+
+/* End materials imports */
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsFeedComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
